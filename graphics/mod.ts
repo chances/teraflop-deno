679f9dc95f52049b34a688622ddad30bbacb4b65
Chance Snow <git@chancesnow.me>
2024-04-11 10:34:11 -0500

    Add a GPU resource interface and class decorator

--- graphics/mod.ts
+++ graphics/mod.ts
@@ -1,18 +1,51 @@
-import { Component } from "../mod.ts";
+import { Component, constantProperty } from "../mod.ts";
 
-export const enum ShaderStage {
-  vertex = "vertex",
-  fragment = "fragment"
+/** A graphics resource with data to initialize in GPU memory. */
+interface Resource {
+  initialize(adapter: GPUAdapter, device: GPUDevice): void;
 }
-export class Shader extends Component {
-  constructor(readonly stage: ShaderStage, readonly source: string) {
-    super();
+
+/**
+ * Decorates a class as a GPU resource.
+ * @see `Resource`
+ **/
+function resource() {
+  // deno-lint-ignore ban-types, no-explicit-any
+  return function (constructor: Function, _: any) {
+    Object.defineProperty(constructor.prototype, "isResource", constantProperty(true));
   }
 }
 
-export class Material extends Component {
+export const enum ShaderStage {
+  vertex = "vertex",
+  fragment = "fragment",
+}
+export @resource() class Shader extends Component implements Resource {
+  private _module: GPUShaderModule | null = null;
+
+  constructor(readonly stage: ShaderStage, readonly source: string, readonly label?: string) {
+    super();
+  }
+
+  get module() {
+    return this._module;
+  }
+
+  initialize(_adapter: GPUAdapter, device: GPUDevice): void {
+    this._module = device.createShaderModule({
+      code: this.source,
+      label: this.label
+    });
+  }
+}
+
+export @resource() class Material extends Component implements Resource {
   constructor(readonly shaders: Shader[], readonly depthTest: boolean) {
     super();
+  }
+
+  initialize(adapter: GPUAdapter, device: GPUDevice): void {
+    this.shaders.forEach(shader => shader.initialize(adapter, device));
   }
 }
 
--- utils.ts
+++ utils.ts
@@ -1,10 +1,23 @@
+// deno-lint-ignore ban-types
 export type SymbolKey<T> = Function | Extract<keyof T, string> | Extract<keyof T, symbol>;
 export function nameof<T>(symbol: SymbolKey<T>) {
   if (typeof symbol === "function") return symbol.prototype.name;
   return symbol;
 }
 
+/** @returns A constant `Object` property descriptor, i.e. the property is not `writable`. */
+// deno-lint-ignore no-explicit-any
+export function constantProperty(value: any): PropertyDescriptor {
+  return {
+    enumerable: true,
+    configurable: false,
+    writable: false,
+    value
+  };
+}
+
 /** Decorates a class such that its constructor and prototype cannot be mutated. */
+// deno-lint-ignore ban-types
 export function sealed(constructor: Function) {
   Object.seal(constructor);
   Object.seal(constructor.prototype);
