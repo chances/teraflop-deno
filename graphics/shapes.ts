f684d925dfc620bc5764e36d4dcdc24fbb8ac35a
Chance Snow <git@chancesnow.me>
2024-04-13 10:11:33 -0500

    Add a colored cube shape

---
+++ graphics/shapes.ts
@@ -0,0 +1,48 @@
+import { Color, VertexPosColor } from "./mod.ts";
+
+// See: https://github.com/deno-windowing/dwm/blob/main/examples/webgpu-cube.ts#L14
+export const cube = [
+  new VertexPosColor([1, -1, 1],   Color.fuschia),
+  new VertexPosColor([-1, -1, 1],  Color.blue),
+  new VertexPosColor([-1, -1, -1], Color.black),
+  new VertexPosColor([1, -1, -1],  Color.red),
+  new VertexPosColor([1, -1, 1],   Color.fuschia),
+  new VertexPosColor([-1, -1, -1], Color.black),
+
+  new VertexPosColor([1, 1, 1],    Color.white),
+  new VertexPosColor([1, -1, 1],   Color.fuschia),
+  new VertexPosColor([1, -1, -1],  Color.red),
+  new VertexPosColor([1, 1, -1],   Color.yellow),
+  new VertexPosColor([1, 1, 1],    Color.white),
+  new VertexPosColor([1, -1, -1],  Color.red),
+
+  new VertexPosColor([-1, 1, 1],   Color.cyan),
+  new VertexPosColor([1, 1, 1],    Color.white),
+  new VertexPosColor([1, 1, -1],   Color.yellow),
+  new VertexPosColor([-1, 1, -1],  Color.green),
+  new VertexPosColor([-1, 1, 1],   Color.cyan),
+  new VertexPosColor([1, 1, -1],   Color.yellow),
+
+  new VertexPosColor([-1, -1, 1],  Color.blue),
+  new VertexPosColor([-1, 1, 1],   Color.cyan),
+  new VertexPosColor([-1, 1, -1],  Color.green),
+  new VertexPosColor([-1, -1, -1], Color.black),
+  new VertexPosColor([-1, -1, 1],  Color.blue),
+  new VertexPosColor([-1, 1, -1],  Color.green),
+
+  new VertexPosColor([1, 1, 1],    Color.white),
+  new VertexPosColor([-1, 1, 1],   Color.cyan),
+  new VertexPosColor([-1, -1, 1],  Color.blue),
+  new VertexPosColor([-1, -1, 1],  Color.blue),
+  new VertexPosColor([1, -1, 1],   Color.fuschia),
+  new VertexPosColor([1, 1, 1],    Color.white),
+
+  new VertexPosColor([1, -1, -1],  Color.red),
+  new VertexPosColor([-1, -1, -1], Color.black),
+  new VertexPosColor([-1, 1, -1],  Color.green),
+  new VertexPosColor([1, 1, -1],   Color.yellow),
+  new VertexPosColor([1, -1, -1],  Color.red),
+  new VertexPosColor([-1, 1, -1],  Color.green),
+];
+
+// TODO: https://github.com/toji/webgpu-bundle-culling/blob/main/js/shapes.js
