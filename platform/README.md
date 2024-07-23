8e77f6908969f2331ceecc6d44d5fd6df50ef3b4
Chance Snow <git@chancesnow.me>
2024-07-22 20:11:39 -0500

    Implement `setIcon` platform wrapper

--- platform/README.md
+++ platform/README.md
@@ -2,6 +2,7 @@
 
 A small, cross-platform library exposing a C API for platform and desktop integrations.
 
+<!-- See https://github.com/googlefonts/noto-emoji/blob/main/png/72/emoji_u1f6a7.png -->
 ## Development
 
 1. Build FFI interface: `make ffi`
--- platform/ffi.ts
+++ platform/ffi.ts
@@ -1,7 +1,53 @@
 import * as ffi from "@gnome/ffi";
+import { assert } from "@std/assert";
+import * as path from "@std/path";
+import * as fs from "@std/fs";
 
-const buf = new Uint8Array([0x0])
+/** Determine library prefix based on the OS. */
+function libPrefix() {
+  switch (Deno.build.os) {
+    case "windows":
+      return "";
+    default:
+      return "lib";
+  }
+}
 
-const ptr = ffi.createPointer(buf);
-console.log(ptr);
-// See https://github.com/gnomejs/sdk/tree/main/ffi
+/** Determine library extension based on the OS. */
+function libSuffix() {
+  switch (Deno.build.os) {
+    case "windows":
+      return "dll";
+    case "darwin":
+      return "dylib";
+    default:
+      return "so";
+  }
+}
+
+// Open library and define exported symbols
+const self = import.meta.url;
+const libName = `${libPrefix()}teraflop-platform.${libSuffix()}`;
+const libPath = path.normalize(
+  path.join(path.dirname(self), `./lib/${libName}`)
+);
+console.log(`Loading Teraflop native platform support extensions...`);
+assert(fs.exists(libPath), `File not found: ${libName}`);
+console.debug(`Reading '${libPath}'`);
+const dylib = Deno.dlopen(libPath,
+  // setIcon(char* filePath)
+  { "setIcon": { parameters: ["pointer"], result: "bool" }} as const,
+);
+console.log(`Loaded.`);
+
+export interface Icon {
+  width: number;
+  height: number;
+  ptr: ffi.IntPtr;
+}
+
+// See https://github.com/gnomejs/sdk/tree/main/ffi#readme
+export function setIcon(filePath: string) {
+  const path = ffi.fromPwstr(filePath);
+  assert(dylib.setIcon(ffi.createPointer(path)), `Could not set icon: ${filePath}`);
+}
