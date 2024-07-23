1b039ea33fa9174614251025d3771d46f456937b
Chance Snow <git@chancesnow.me>
2024-07-22 23:46:13 -0500

    Integrate mobx into server
    https://mobx.js.org

--- debugger/server.ts
+++ debugger/server.ts
@@ -1,5 +1,6 @@
 /// <reference no-default-lib="true" />
 /// <reference lib="deno.worker" />
+import * as mobx from "mobx";
 import * as fs from "@std/fs";
 import * as path from "@std/path";
 
@@ -8,20 +9,28 @@ import { try_files } from "https://deno.land/x/try_files/mod.ts";
 
 const location = new URL(import.meta.url);
 const decoder = new TextDecoder("utf-8");
+const state = mobx.makeAutoObservable({
+  title: "Teraflop Debugger",
+  location
+});
 
 // TODO: Pick a unique port for Teraflop
 export const port = 8080;
 
 self.onmessage = (e: MessageEvent) => {
   console.log(e.data);
+  // TODO: SSE for app events?
 };
 
 const publicPath = import.meta.resolve("./public");
 const appJsPath = path.join(publicPath, "app.js");
 // decoder.decode(await Deno.readFile(appJsPath))
 
-// TODO: Server-side SSR with state management, i.e. mobx
-const document = html`
+// Server-side SSR with state management
+function render() {
+  return html`
+<!-- FIXME: Send title changes back to the webview -->
+<title>${state.title}</title>
 <style>body { font-family: sans-serif; }</style>
 <body>
   <h1>Hello from deno v${Deno.version.deno}</h1>
@@ -29,15 +38,15 @@ const document = html`
   <p><a href="https://basecamp.com">Basecamp</a></p>
 </body>
 <script type="text/javascript" src="http://localhost:${port}/app.js"></script>
-`;
+  `;
+}
+// FIXME: mobx.autorun(() => document = render());
 
 try_files(async function(request) {
   let { pathname } = new URL(request.url);
 
-  if (pathname === '/') return new Response(document, {
-    headers: {
-      "Content-Type": "text/html"
-    }
+  if (pathname === '/') return new Response(render(), {
+    headers: { "Content-Type": "text/html" }
   });
   return new Response(`File not found: ${new URL(request.url).pathname}` ,{status:404});
 }, {
